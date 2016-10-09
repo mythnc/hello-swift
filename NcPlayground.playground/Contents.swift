1673e65241548626a97c01a262cb433577696b5d
N.C Wu <mythnc@livemail.tw>
2016-10-09 19:47:27 +0800

    Add variable practice

---
+++ NcPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "nc wu"
+
+print(name)
+
+var myInt = 42
+
+print(myInt + 8)
+
+myInt += 1
+
+myInt / 5
+myInt % 5
+
+
+print("myInt: \(myInt)")
+
+let age = 1
+print("my name is \(name) and I am \(age) years old")
+
+var a: Double = 8.73
+var b: Float = 8.73
+var c = 3.2
+print(a / c)
+print(Int(a))
+
+let d = 5.76
+let i = 8
+print(d * Double(i))
