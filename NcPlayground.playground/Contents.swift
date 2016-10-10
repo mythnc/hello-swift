7b6a98a7cd9c2e1cfa26456a30f7586a404d8952
N.C Wu <mythnc@livemail.tw>
2016-10-10 20:41:45 +0800

    While practice

--- NcPlayground.playground/Contents.swift
+++ NcPlayground.playground/Contents.swift
@@ -2,5 +2,23 @@
 
 import UIKit
 
-let n = arc4random_uniform(6)
-print(n)
+var i = 1
+
+while i < 10 {
+    print(i)
+    i += 1
+}
+
+i = 1
+while i <= 20 {
+    print(i * 7)
+    i += 1
+}
+
+var array = [7, 23, 98, 1, 0, 763]
+i = 0
+while i < array.count{
+    array[i] += 1
+    i += 1
+}
+print(array)
