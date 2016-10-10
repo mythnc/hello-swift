31d57de03159c5810fea0d6743289e834db44764
N.C Wu <mythnc@livemail.tw>
2016-10-10 13:29:16 +0800

    if else statement practice

--- NcPlayground.playground/Contents.swift
+++ NcPlayground.playground/Contents.swift
@@ -2,36 +2,33 @@
 
 import UIKit
 
-// Array
-var array = [35, 36, 1, 0]
-print(array[0])
-print(array.count)
-array.append(3)
-array.sort()
+let age = 13
 
-var ar = [3.87, 7.1, 8.9]
-ar.remove(at: 1)
-ar.append(ar[0] * ar[1])
+if age < 18 {
+    print("under 18 gg")
+} else {
+    print("above 18")
+}
 
-let mixAr = [3.5, 3, "hello", false] as [Any]
-var strAr = [String]()
-strAr.append("3")
+let name = "ncwu"
 
-// Dictionary
-var dict = ["computer": "intel",
-"coffee": "energy"]
-print(dict["computer"]) // Optional("intel")
-print(dict["computer"]!) // intel
-// print(dict["error"]!) error
+if name == "ncwu" {
+    print("hi nc")
+} else {
+    print("hello there")
+}
 
-print(dict.count)
-dict["phone"] = "samsung"
-dict.removeValue(forKey: "coffee")
-print(dict)
+if name == "ncwu" && age >= 20 {
+    print("nc, you are above 18")
+} else if name == "ncwu" {
+    print("nc, you are too young")
+}
 
-var charDict = [String: Double]()
-charDict["123"] = 12.3
+let username = "test"
+let password = "pwd"
 
-var menuDict = [String: Double]()
-menuDict = ["pizza": 10.99, "ice cream": 4.99, "salad": 7.99]
-print("The total cost of my meal is \(menuDict["pizza"]! + menuDict["salad"]!)")
+if username == "test" && password == "pwd" {
+    print("successul login")
+} else {
+    print("login failed")
+}
