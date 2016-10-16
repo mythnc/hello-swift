7fede56191721a1e2fb71e58e214bd91929bd921
N.C Wu <mythnc@livemail.tw>
2016-10-16 15:35:02 +0800

    Navigation Bars practice

---
+++ Menu-Bars/Menu-Bars/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Menu-Bars
+//
+//  Created by N.C on 2016/10/16.
+//  Copyright © 2016年 N.C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func cameraButton(_ sender: AnyObject) {
+        print("camera button press!")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
