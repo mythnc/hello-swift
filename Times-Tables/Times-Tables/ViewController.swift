0a15c2848a4bd763a4900c70f5145bf2152bb089
N.C Wu <mythnc@livemail.tw>
2016-10-16 19:59:52 +0800

    Slider + Table Views exercise

--- Times-Tables/Times-Tables/ViewController.swift
+++ Times-Tables/Times-Tables/ViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var table: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String((indexPath.row + 1) * Int(floor(slider.value)))
+        return cell
+    }
     
     @IBAction func sliderChanged(_ sender: AnyObject) {
-        print(slider.value)
+        table.reloadData()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
