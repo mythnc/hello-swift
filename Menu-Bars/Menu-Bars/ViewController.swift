fce0a64e175ab15d7285b83274050ce1da148f8d
N.C Wu <mythnc@livemail.tw>
2016-10-16 17:01:35 +0800

    Add egg timer example

--- Menu-Bars/Menu-Bars/ViewController.swift
+++ Menu-Bars/Menu-Bars/ViewController.swift
@@ -9,8 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBAction func cameraButton(_ sender: AnyObject) {
-        print("camera button press!")
+    let START_TIME = 210
+    var timer = Timer()
+    var time = 210
+    var isPlayed = false
+    @IBOutlet weak var eggTime: UILabel!
+    
+    func decreaseTimer() {
+        time -= 1
+        if time >= 0 {
+            eggTime.text = String(time)
+        }
+    }
+    
+    @IBAction func playButton(_ sender: AnyObject) {
+        if !isPlayed {
+            isPlayed = true
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func pauseButton(_ sender: AnyObject) {
+        isPlayed = false
+        timer.invalidate()
+    }
+    
+    @IBAction func plus10secButton(_ sender: AnyObject) {
+        self.adjustTimeView(time: time + 10)
+    }
+    
+    @IBAction func minus10secButton(_ sender: AnyObject) {
+        if time >= 10 {
+            self.adjustTimeView(time: time - 10)
+        }
+    }
+    
+    @IBAction func resetButton(_ sender: AnyObject) {
+        self.adjustTimeView(time: self.START_TIME)
+    }
+    
+    func adjustTimeView(time: Int) {
+        self.time = time
+        eggTime.text = String(time)
     }
 
     override func viewDidLoad() {
@@ -22,7 +62,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
