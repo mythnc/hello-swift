2bdecd1ac3bcfa29aa3429f2f24a9ac8e7aff64d
N.C Wu <mythnc@livemail.tw>
2016-10-18 23:08:15 +0800

    Add "add to-do item" logic

--- To-Do-List/To-Do-List/SecondViewController.swift
+++ To-Do-List/To-Do-List/SecondViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var todoTextField: UITextField!
 
+    @IBAction func addTodoButton(_ sender: AnyObject) {
+        self.view.endEditing(true)
+        addTodo()
+    }
+    
+    func addTodo() {
+        if let todoString = todoTextField.text {
+            if todoString != "" {
+                print("todo: \(todoString)")
+                todoTextField.text = ""
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +35,16 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // turn off the keyboard
+        textField.resignFirstResponder()
+        addTodo()
+        return true
+    }
 }
 
