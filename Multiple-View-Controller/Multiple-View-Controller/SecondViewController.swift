504aa73881e3dced1bed9602602f4dde9c088a11
N.C Wu <mythnc@livemail.tw>
2016-10-18 22:10:41 +0800

    Multiple view controller example

---
+++ Multiple-View-Controller/Multiple-View-Controller/SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  Multiple-View-Controller
+//
+//  Created by N.C on 2016/10/18.
+//  Copyright © 2016年 N.C. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("second view controller loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Multiple-View-Controller/Multiple-View-Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiple-View-Controller
+//
+//  Created by N.C on 2016/10/18.
+//  Copyright © 2016年 N.C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
