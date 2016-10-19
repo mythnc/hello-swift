2c94a7d89919fb98c2cc684fd0a135cce15e3295
N.C Wu <mythnc@livemail.tw>
2016-10-19 22:04:39 +0800

    Add app template

---
+++ Weather-Query/Weather-Query/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weather-Query
+//
+//  Created by N.C on 2016/10/19.
+//  Copyright © 2016年 N.C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg.png")!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
