e12e415d59dca406d0e1304525047946950148ae
N.C Wu <mythnc@livemail.tw>
2016-10-21 08:16:26 +0800

    Link query weather logic with view

--- Weather-Query/Weather-Query/ViewController.swift
+++ Weather-Query/Weather-Query/ViewController.swift
@@ -26,7 +26,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func queryLocation(location: String) {
         let queryName = location.replacingOccurrences(of: " ", with: "-")
-        print(queryName)
         if let url = URL(string :"http://weather-forecast.com/locations/\(queryName)/forecasts/latest") {
             let request = NSMutableURLRequest(url: url)
             let task = URLSession.shared.dataTask(with: request as URLRequest) {
@@ -36,8 +35,32 @@ class ViewController: UIViewController, UITextFieldDelegate {
                     print(error)
                 } else {
                     if let unwrappedData = data {
-                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                        print(dataString)
+                        let dataObject = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        let queryFailed = "mistyped the address"
+                        let summaryTitle = "Weather Forecast Summary:</b>"
+                        
+                        if let dataString = dataObject {
+                            if dataString.contains(queryFailed) {
+                                DispatchQueue.main.sync(execute: {
+                                    self.weatherSummary.text = "The weather there couldn't be found. Please try again."
+                                })
+                            } else if dataString.contains(summaryTitle) {
+                                let substringHead = dataString.range(of: summaryTitle)
+                                let summaryStart = NSString(string: dataString.substring(from: substringHead.location + summaryTitle.characters.count))
+                                
+                                let spanStartTag = "<span class=\"phrase\">"
+                                let spanStart = summaryStart.range(of: spanStartTag)
+                                let descriptionStart = NSString(string: summaryStart.substring(from: spanStart.location + spanStartTag.characters.count))
+                                
+                                let spanEndTag = "</span>"
+                                let spanEnd = descriptionStart.range(of: spanEndTag)
+                                let description = descriptionStart.substring(to: spanEnd.location)
+                                
+                                DispatchQueue.main.sync(execute: {
+                                    self.weatherSummary.text = description.replacingOccurrences(of: "&deg;", with: "°")
+                                })
+                            }
+                        }
                     }
                 }
             }
