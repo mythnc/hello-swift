84f2a18672feddbf2dcbcfaa6a08b1f84f8d615f
N.C Wu <mythnc@livemail.tw>
2016-10-23 15:34:08 +0800

    Auto-play animation

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -10,17 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     @IBOutlet weak var image: UIImageView!
-    var i = 1
+    var i = 0
     let IMAGE_NUMBER = 35
+    var isStrted = false
+    var timer = Timer()
     
-    @IBAction func next(_ sender: AnyObject) {
+    func play() {
         image.image = UIImage(named: "frame_\(i)_delay-0.1s.gif")
         i += 1
         if i == IMAGE_NUMBER {
             i = 0
         }
     }
+    
+    @IBAction func animate(_ sender: AnyObject) {
+        isStrted = !isStrted
+        if isStrted {
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.play), userInfo: nil, repeats: true)
+            button.setTitle("Stop", for: [])
+        } else {
+            timer.invalidate()
+            button.setTitle("Start", for: [])
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
