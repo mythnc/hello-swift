452fc7d7d56a29e9ef9c263a89404deda2f910ce
N.C Wu <mythnc@livemail.tw>
2016-10-24 07:57:00 +0800

    Add win logic

--- tic-tac-toe/tic-tac-toe/ViewController.swift
+++ tic-tac-toe/tic-tac-toe/ViewController.swift
@@ -18,62 +18,127 @@ class ViewController: UIViewController {
     @IBOutlet weak var button6: UIButton!
     @IBOutlet weak var button7: UIButton!
     @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var winMsg: UILabel!
     
     let crossImg = UIImage(named: "cross.png")
     let noughtImg = UIImage(named: "nought.png")
     var isNought = false
     var isPressed = Array(repeating: false, count: 9)
+    var record = Array(repeating: "", count: 9)
+    var isFinished = false
     var buttons: [UIButton] = [UIButton]()
     
     func setButtonImage(i: Int) {
-        if isPressed[i] {
+        if isPressed[i] || isFinished {
             return
         }
         
         isPressed[i] = true
         if !isNought {
             buttons[i].setBackgroundImage(noughtImg, for: .normal)
+            record[i] = "O"
             isNought = true
             return
         }
         isNought = false
         buttons[i].setBackgroundImage(crossImg, for: .normal)
+        record[i] = "X"
+    }
+    
+    func isWin() -> Bool {
+        let winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
+                            [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        for winCondition in winConditions {
+            if record[winCondition[0]] == record[winCondition[1]]
+                && record[winCondition[1]] == record[winCondition[2]]
+                && record[winCondition[0]] != "" {
+                isFinished = true
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    @IBAction func reset(_ sender: AnyObject) {
+    }
+    
+    func displayWinMsg(i: Int) {
+        winMsg.center = CGPoint(x: winMsg.center.x - 500, y: winMsg.center.y)
+        
+        UIView.animate(withDuration: 1) {
+            self.winMsg.center = CGPoint(x: self.winMsg.center.x + 500, y: self.winMsg.center.y)
+        }
+        
+        if record[i] == "O" {
+            winMsg.text = "Nought has won!"
+            return
+        }
+        winMsg.text = "Cross has won!"
     }
     
     @IBAction func buttonLeftTop(_ sender: AnyObject) {
         setButtonImage(i: 0)
+        if isWin() {
+            displayWinMsg(i: 0)
+        }
     }
     
     @IBAction func buttonTop(_ sender: AnyObject) {
         setButtonImage(i: 1)
+        if isWin() {
+            displayWinMsg(i: 1)
+        }
     }
     
     @IBAction func buttonRightTop(_ sender: AnyObject) {
         setButtonImage(i: 2)
+        if isWin() {
+            displayWinMsg(i: 2)
+        }
     }
     
     @IBAction func buttonLeft(_ sender: AnyObject) {
         setButtonImage(i: 3)
+        if isWin() {
+            displayWinMsg(i: 3)
+        }
     }
     
     @IBAction func buttonCenter(_ sender: AnyObject) {
         setButtonImage(i: 4)
+        if isWin() {
+            displayWinMsg(i: 4)
+        }
     }
     
     @IBAction func buttonRight(_ sender: AnyObject) {
         setButtonImage(i: 5)
+        if isWin() {
+            displayWinMsg(i: 5)
+        }
     }
     
     @IBAction func buttonDownLeft(_ sender: AnyObject) {
         setButtonImage(i: 6)
+        if isWin() {
+            displayWinMsg(i: 6)
+        }
     }
     
     @IBAction func buttonDown(_ sender: AnyObject) {
         setButtonImage(i: 7)
+        if isWin() {
+            displayWinMsg(i: 7)
+        }
     }
     
     @IBAction func buttonDownRight(_ sender: AnyObject) {
         setButtonImage(i: 8)
+        if isWin() {
+            displayWinMsg(i: 8)
+        }
     }
     
     override func viewDidLoad() {
